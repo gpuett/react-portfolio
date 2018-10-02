9fcca9f5925d68ca79ec16565a4cbae117e5ddbc
Garnett Puett <gpuett@gmail.com>
2018-10-02 12:22:31 -0700

    add metacritic rebuild image

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Pour from '../images/Pour.png'
+import meta from '../images/meta.png'
 
 import Layout from '../components/layout'
 
@@ -9,6 +10,9 @@ const SecondPage = () => (
     <h1>Projects</h1>
     <a href='https://github.com/gpuett/help-me-im-pour'><h3>Pour</h3></a>
     <img src={Pour} />
+    <br/>
+    <a href='https://github.com/gpuett/site-rebuild'><h3>MetaCritic Rebuild</h3></a>
+    <img src={meta} />
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
