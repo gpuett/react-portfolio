be3c7b6bb021fcd689d8e3fcecf1466c777210aa
Garnett Puett <gpuett@gmail.com>
2018-10-02 12:41:06 -0700

    add collaboration projects

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Link } from 'gatsby'
 import Pour from '../images/Pour.png'
 import meta from '../images/meta.png'
+import jddb from '../images/jddb.png'
+import board from '../images/board.png'
 
 import Layout from '../components/layout'
 
@@ -13,6 +15,11 @@ const SecondPage = () => (
     <br/>
     <a href='https://github.com/gpuett/site-rebuild'><h3>MetaCritic Rebuild</h3></a>
     <img src={meta} />
+    <h1>Collaborations</h1>
+    <a href='https://github.com/gpuett/dating-app'><h3>JDDB</h3></a>
+    <img src={jddb} />
+    <a href='https://github.com/gpuett/boulderingGenerator'><h3>Build-a-Board</h3></a>
+    <img src={board} />
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
